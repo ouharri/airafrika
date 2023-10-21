c742ee8bae2332f5f9e594a21ae0e9eec85740d3
ouharri <ouharrioutman@gmail.com>
2023-10-21 04:34:35 +0100

    Configure CDI

--- src/main/java/org/airafrika/App/Controllers/HelloController.java
+++ src/main/java/org/airafrika/App/Controllers/HelloController.java
@@ -3,6 +3,8 @@ package org.airafrika.App.Controllers;
 import java.io.*;
 import java.time.LocalDate;
 
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
 import org.airafrika.App.Enums.Gender;
 import org.airafrika.Dao.AdminDao;
 import org.airafrika.Dao.ReservationExtraDao;
@@ -10,29 +12,18 @@ import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 import org.airafrika.App.Entities.Admin;
 
+@RequestScoped
 @WebServlet(name = "helloController", value = "/hello")
 public class HelloController extends HttpServlet {
     private String message;
 
+    @Inject
+    private AdminDao adminDao;
+
     public void init() {
         message = "Hello World!";
 
-        Admin admn = new Admin();
-        admn.setFirstName("5daama");
-        admn.setLastName("lehna");
-        admn.setEmail("ouharrioutman@gmail.com");
-        admn.setBirthday(LocalDate.parse("1999-01-01"));
-        admn.setGender(Gender.MALE);
-        admn.setCnie("AD334799");
-        admn.setPassword("jfhjhf");
-
-//        new ReservationExtraDao().getAll().forEach((e) -> {
-//            System.out.println("\n");
-//            System.out.println(e.toString());
-//            System.out.println("\n");
-//        });
-
-        new AdminDao().getAll().forEach((e) -> {
+        adminDao.getAll().forEach((e) -> {
             System.out.println("\n");
             System.out.println(e.toString());
             System.out.println("\n");
