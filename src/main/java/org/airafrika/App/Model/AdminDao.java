5a58731b1c3431e91d7392ee253109f236c32743
ouharri <ouharrioutman@gmail.com>
2023-10-26 10:55:52 +0100

    progress 5%

--- src/main/java/org/airafrika/App/Model/AdminDao.java
+++ src/main/java/org/airafrika/App/Model/AdminDao.java
@@ -9,11 +9,4 @@ import java.util.Optional;
 
 @Model
 public class AdminDao extends Dao<Admin> {
-
-    public Optional<Admin> findByEmailAndPassword(String email, String password) {
-        return Optional.ofNullable( em.createQuery("SELECT a FROM Admin a WHERE a.email = :email AND a.password = :password",Admin.class)
-                .setParameter("email", email)
-                .setParameter("password", password)
-                .getSingleResult());
-    }
 }
